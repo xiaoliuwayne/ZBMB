b120669a71aeaca8fdbcadd41e4adaff12d96fea
wayne <1005881270@qq.com>
2019-04-02 17:05:58 +0800

    样式调整，然后就是跳转方法增加传参需求单id

--- src/pages/orders/OrdersList.vue
+++ src/pages/orders/OrdersList.vue
@@ -1,43 +1,52 @@
 <template>
 <div>
   <h3 class="main-bk">{{ title }}</h3>
+  <!--<van-nav-bar-->
+    <!--title="找布买布"-->
+    <!--left-arrow-->
+    <!--@click-left="back"-->
+    <!--class="main-bk header"-->
+  <!--/>-->
   <van-tabs @click="onClick" color="#1aad19">
     <van-tab title="待接单">
-      <div class="card" v-for="(item,hh) in showDatasWait" :key="hh" @click="goRequirement('w')">
-        <span>
+      <div class="card" v-for="(item,hh) in showDatasWait" :key="hh" @click="goRequirement('w',item.inquiryId)">
+        <div style="width: 20vw"  align="center">
           <img :src="item.imgUrl" alt="样布图片"/>
-        </span>
-        <span>
-          <p>{{item.createTime}}<span style="color: red;float: right;margin-top: 0;">{{item.expireTime}}</span></p>
-          <p>面料种类：{{item.clothType}}</p>
-          <p>说明：{{item.desc}}</p>
-        </span>
-        <span style="width: 70px">
-          <p style="margin: 2px 15px">{{item.status}}</p>
-        </span>
+        </div>
+        <div style="width: 70vw;">
+          <p><span class="card-div-span">{{item.createTime}}</span>
+            <span style="color: red;float: right;" class="card-div-span">{{item.expireTime}}</span>
+          </p>
+          <p class="card-div-p">面料种类：{{item.clothType}}</p>
+          <p class="card-div-p">说明：{{item.desc}}</p>
+        </div>
+        <div style="width: 14vw;">
+          <p class="card-div-span" style="text-align: center">{{item.status}}</p>
+        </div>
       </div>
     </van-tab>
     <van-tab title="已接单">
       <div v-for="(item,gg) in showDatas" :key="gg" class="get-order">
         <div class="card" style="margin-bottom: 0" >
-          <span>
+          <div style="width: 20vw"  align="center">
             <img :src="item.imgUrl" alt="样布图片"/>
-          </span>
-          <span>
-            <p>{{item.createTime}}</p>
-            <p>面料种类：{{item.clothType}}</p>
-            <p>说明：{{item.desc}}</p>
-          </span>
-          <span style="width: 70px">
-            <p style="margin: 5px">{{item.status}}</p>
-          </span>
+          </div>
+          <div style="width: 70vw;">
+            <p><span class="card-div-span">{{item.createTime}}</span>
+            </p>
+            <p class="card-div-p">面料种类：{{item.clothType}}</p>
+            <p class="card-div-p">说明：{{item.desc}}</p>
+          </div>
+          <div style="width: 14vw;">
+            <p class="card-div-span" style="text-align: center">{{item.status}}</p>
+          </div>
         </div>
         <div class="dot-line"></div>
         <div class="feedback">
           <span>色卡编号：{{item.colorCode}}</span>
           <span>单价：{{item.price}}</span>
           <span>
-            <van-button @click="goRequirement('g')" class="bt-bright bt-check" >
+            <van-button @click="goRequirement('g',item.inquiryId)" class="bt-bright bt-check" >
               查看
             </van-button>
           </span>
@@ -49,53 +58,36 @@
 </template>
 
 <script>
-import {STATUS, formatDate} from '../../assets/js/common.js'
+import {STATUS, formatDate, CLOTHSTYLE} from '../../assets/js/common.js'
 export default {
   data () {
     return {
       index2responsed: {0: 0, 1: 1},
       title: '找布买布',
       page: 0,
-      pageSize: 20,
+      pageSize: 20, // 一页20条
       // responsed: 1,
       providerId: 0,
+      inquiryId: 0,
       showDatas: [],
-      showDatasWait: [],
-      ordersDataWait: [
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:40', 'deadline': '14天后清除', 'status': '待接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:41', 'deadline': '14天后清除', 'status': '待接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:42', 'deadline': '14天后清除', 'status': '待接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:43', 'deadline': '14天后清除', 'status': '待接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:44', 'deadline': '14天后清除', 'status': '待接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:45', 'deadline': '14天后清除', 'status': '待接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:46', 'deadline': '14天后清除', 'status': '待接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...'}
-      ],
-      ordersData: [
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:47', 'deadline': '', 'status': '已接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...', 'colorCode': 'ASD12365478', 'price': '10.00/米'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:48', 'deadline': '', 'status': '已接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...', 'colorCode': 'ASD12365478', 'price': '10.00/米'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:49', 'deadline': '', 'status': '已接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...', 'colorCode': 'ASD12365478', 'price': '10.00/米'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:50', 'deadline': '', 'status': '已接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...', 'colorCode': 'ASD12365478', 'price': '10.00/米'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:51', 'deadline': '', 'status': '已接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...', 'colorCode': 'ASD12365478', 'price': '10.00/米'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:52', 'deadline': '', 'status': '已接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...', 'colorCode': 'ASD12365478', 'price': '10.00/米'},
-        {'img': require('../../assets/zsi.png'), 'dateTime': '2019.3.22 16:53', 'deadline': '', 'status': '已接单', 'type': '一级分类、二级分类', 'remark': '请描述要找的主要特征、品名、成分、工艺、用途、规格等请描...', 'colorCode': 'ASD12365478', 'price': '10.00/米'}
-      ]
+      showDatasWait: []
     }
   },
   created () {
     this.providerId = this.$route.params.providerId
+    this.inquiryId = this.$route.params.inquiryId
     this.init()
   },
   methods: {
     init () {
-      this.getHttpData(1)
+      this.getHttpData(0)
     },
     getHttpData (responsedId) {
       let url = '/tsebuapi/show.do?'
-      this.providerId = 28191
+      // this.providerId = 28191
       let param = {
         'sendProviderId': this.providerId,
         'responsed': responsedId
-        // 'inquiryId': 7
       }
       let formData = {
         'cmd': 'queryInquiryList',
@@ -112,6 +104,7 @@ export default {
           let response = res.data
           if (response.exId) {
             alert(response.exDesc)
+            // this.back()
           } else {
             console.log('orderList=>response', response)
             this.setShowDatas(response, responsedId)
@@ -121,33 +114,40 @@ export default {
         console.log('error', error)
       })
     },
+    getClothType (keyWords) { // 设置面料种类
+      if (keyWords) {
+        let first = CLOTHSTYLE[keyWords[0].groupId]
+        let second = keyWords[0].keyword
+        return first + '-' + second
+      }
+    },
     setShowDatas (response, id) {
       this.showDatas = []
       this.showDatasWait = []
       response.list.forEach(obj => {
         let tmpCreate = formatDate(obj.createTime)
         let tmpExpire = formatDate(obj.expireTime)
-        if (id === 0) { // (id === 1 暂时改为 0 以测试
+        if (id === 1) { // (id === 1 暂时改为 0 以测试
           this.showDatas.push({
+            'inquiryId': obj.inquiryId,
             'imgUrl': obj.imageList[0],
             'createTime': tmpCreate,
             'expireTime': tmpExpire,
             'status': STATUS[1], // 是否接单，obj.status
-            'clothType': '一级分类、二级分类', // obj.clothType,
-            'desc': '水泥地上的是第四代是多少都会山东', // obj.desc
-            'colorCode': 'ASD12365478', // obj.colorCode
-            'price': '10/米' // obj.price
+            'clothType': this.getClothType(obj.keywords), // obj.clothType,
+            'desc': obj.desc, // obj.desc
+            'colorCode': obj.colorCardCode, // obj.colorCode
+            'price': String(obj.price) + '/米' // obj.price
           })
         } else {
           this.showDatasWait.push({
+            'inquiryId': obj.inquiryId,
             'imgUrl': obj.imageList[0],
             'createTime': tmpCreate,
             'expireTime': tmpExpire,
-            'status': STATUS[1], // 是否接单，obj.status
-            'clothType': '一级分类、二级分类', // obj.clothType,
-            'desc': '水泥地上的是第四代是多少都会山东', // obj.desc
-            'colorCode': 'ASD12365478', // obj.colorCode
-            'price': '10/米' // obj.price
+            'status': STATUS[0], // 是否接单，obj.status
+            'clothType': this.getClothType(obj.keywords), // obj.clothType,
+            'desc': obj.desc // obj.desc
           })
         }
       })
@@ -158,11 +158,14 @@ export default {
       console.log('index, title', index, title)
       this.getHttpData(this.index2responsed[index])
     },
-    goRequirement (flag) {
+    goRequirement (flag, inquiryId) {
       this.$router.push({name: 'Requirement',
         params: {'flag': flag,
           'providerId': this.providerId,
-          'inquiryId': 7}})
+          'inquiryId': inquiryId}})
+    },
+    back () {
+      this.$router.go(-1)
     }
   }
 }
