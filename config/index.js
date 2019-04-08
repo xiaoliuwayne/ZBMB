2d9e9629358f7145608106c4e25df8484e0a4b4f
wayne <1005881270@qq.com>
2019-04-08 09:32:23 +0800

    20190408-删除注释，没有用的代码而且开始大改动结构

--- config/index.js
+++ config/index.js
@@ -29,8 +29,9 @@ module.exports = {
       },
     },
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    // host: '192.168.50.230', // can be overwritten by process.env.HOST
+    // host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '172.20.10.14', // can be overwritten by process.env.HOST
+    host: '192.168.50.230', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
