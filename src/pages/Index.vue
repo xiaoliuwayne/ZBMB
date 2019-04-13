79a7bc8448514726e394866389659752c429c5e8
wayne <1005881270@qq.com>
2019-04-13 16:10:41 +0800

    路由传参

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,24 +3,21 @@
     <div style="text-align:center; margin:3rem 0;">
       <img src="../assets/img/bk/zbmblogo.png" style="width: 30vw;height: 30vw;">
     </div>
-    <div class="aa">
+    <div class="index-p">
       <span>
         <img src="../assets/img/bk/idea.png" style="width: 3rem; height: 3rem"/>
       </span>
-      <span class="bb">
+      <span class="index-remind">
         您已获得一个面料采购订单，请将短信中的“提取码”输入下方输入框中，点击“获得订单”查看订单。
       </span>
     </div>
-    <!--<van-cell-group>-->
-      <!--<van-field v-model="code" placeholder="输入提取码" class="code-input" type='number' input-align="left" label-align="left"/>-->
-      <input v-model="code" placeholder="输入提取码" class="code-input" type='number'/>
-    <!--</van-cell-group>-->
+    <input v-model="code" placeholder="输入提取码" class="code-input" type='number'/>
     <div style="text-align:center; padding:30px 0;">
     <van-button @click="sendCode" class="code-send">
       获得订单
     </van-button>
     </div>
-    <div class="cc">
+    <div class="index-footer">
       <p style="font-weight: bolder">找布买布 平台介绍：</p>
       <p>“找布买布” 服务于面料采购和面料档口，将双方需求进行汇聚到平台中，进行自由交易。</p>
     </div>
@@ -40,7 +37,7 @@ export default {
     }
   },
   created () {
-    // 防止不小心退出后重输验证码
+    // 防止不小心退出后要重输验证码
     let tmpCode = sessionStorage.getItem('code')
     if (tmpCode) {
       this.code = tmpCode
@@ -50,11 +47,9 @@ export default {
   },
   methods: {
     sendCode () {
-      console.log('index ==this.code,', this.code)
-      // localStorage.setItem('code', String(this.code))
       sessionStorage.setItem('code', String(this.code))
       if (this.code) {
-        this.$router.push({name: 'Main', params: {'code': String(this.code)}})
+        this.$router.push({name: 'Main'})
       }
     }
   }
@@ -63,49 +58,4 @@ export default {
 
 <style scoped>
 @import '../assets/css/mycss.css';
-
-  .aa{
-    display: table;
-    /*margin: 22vw 0 0 7.5vw;*/
-    padding:1rem 1rem;
-    background: rgba(0,0,0,0.4);
-    /*display: table-cell;*/
-    vertical-align:middle;
-    /*width: 100vw;*/
-    /*width: 100%;*/
-    display:flex;
-    align-items:center;
-  }
-  .aa span{
-    display:flex;
-  }
-  .bb{
-    color:#fff;
-    font-size:0.9rem;
-    padding:0 1rem;
-    /*display: ;*/
-    /*top: 0;*/
-    /*font-size: 4vw;*/
-    /*line-height: 5vw;*/
-    /*color: white;*/
-    /*width: 67vw;*/
-    /*height: 15vw;*/
-    /*margin-left: 5vw;*/
-  }
-  .cc{
-    /*position: fixed;*/
-    /*bottom: 5vw;*/
-    /*margin: 10vw 6vw;*/
-    flex:1;
-    padding:2rem 1.5rem;
-    font-size:0.9rem;
-    display:flex;
-    flex-direction:column;
-    justify-content:flex-end;
-  }
-  .cc p {
-    color: white;
-    font-size: 4vw;
-    margin: 0;
-  }
 </style>
